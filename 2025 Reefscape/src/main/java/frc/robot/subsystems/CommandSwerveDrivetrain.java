4db3d8404fe615bc957b7409ecf151ff30101fc1
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2025-03-25 19:46:10 -0400

    Start Adding Auto Align Code
    Still has errors with drivetrain

--- 2025 Reefscape/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ 2025 Reefscape/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -17,6 +17,7 @@ import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
 import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.numbers.N1;
